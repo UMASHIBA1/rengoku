c958622befa2c3ba37e46bb5a6e40a28492d5a87
UMASHIBA <umashiba1@gmail.com>
2022-01-25 16:48:25 +0900

    fix: fix wat_to_wasm

--- src/utils/wat_to_wasm.rs
+++ src/utils/wat_to_wasm.rs
@@ -1,18 +1,15 @@
 use std::path::Path;
 use std::fs::{read, write, create_dir_all};
 
+
+// NOTE: the file path expect path from directory root(e.g. ./src/loader/test_data/simple_module.wat)
 pub fn wat_to_wasm(input_path: String, output_path: String) {
 
     let binary = wat::parse_file(&input_path);
 
     match binary {
         Ok(binary) => {
-
-            let create_dir_result = create_dir_all(&output_path);
-            if create_dir_result.is_err() {
-                panic!("failed to create dirs {}", output_path)
-            }
-
+            
             let write_result = write(&output_path, binary);
             if write_result.is_err() {
                 panic!("failed to write {}", output_path)
